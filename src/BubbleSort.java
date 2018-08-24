88f1cdf6dcd13b3a0970fbc6603827dedd41fde1
Joseph <jlaos1234@yahoo.com>
2018-08-24 04:28:35 -0700

    Added ascending and descending methods.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,49 +1,112 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 
+/***************
+ * Bubble sort -- both ascendng and descending
+ */
 public class BubbleSort
 {
-   static public void sort(int[] nums)
+   boolean printAfterPass;
+   BubbleSort(boolean printAfterPass) { this.printAfterPass = printAfterPass; }
+
+   BubbleSort(){ this.printAfterPass = false; }
+
+   public void sortAscending(int[] nums)
    {
       boolean swapped;
-
-      do
+      for(int j = nums.length - 1; j > 1; j--)
       {
-         swapped = false;
          for (int i = 0; i < nums.length - 1; i++)
          {
             if (nums[i] > nums[i + 1])
             {
-               int temp = nums[i];
-               nums[i] = nums[i + 1];
-               nums[i + 1] = temp;
-               swapped = true;
+               swap(nums, i);
             }
          }
-      }while (swapped == true);
+         if (printAfterPass)
+         {
+            System.out.println("PASS STARTED AT : " + j);
+            printArray(nums);
+         }
+      }
+   }
+
+   public void sortDescending(int[] nums)
+   {
+      boolean swapped;
+      for(int j = nums.length - 1; j > 1; j--)
+      {
+         for (int i = 0; i < nums.length - 1; i++)
+         {
+            if (nums[i] < nums[i + 1])
+            {
+               swap(nums, i);
+            }
+         }
+         if (printAfterPass)
+         {
+            System.out.println("PASS STARTED AT : " + j);
+            printArray(nums);
+         }
+      }
+   }
+
+   private static void swap(int[] nums, int i)
+   {
+      int temp = nums[i];
+      nums[i] = nums[i + 1];
+      nums[i + 1] = temp;
+   }
+
+   private static void printArray(int[] nums)
+   {
+      for (int i : nums)
+      {
+         System.out.print(i + " ");
+      }
+      System.out.println("\n");
+   }
+   private static int[] getRandomArray(int size, int bound)
+   {
+      int[] nums = new int[size];
+      Random ran = new Random();
+
+      for (int i = 0; i < size; i++)
+      {
+         nums[i] = ran.nextInt(bound);
+      }
+      return nums;
    }
 
    public static void main(String[] args)
    {
-      int size = 100;
-      int[] nums = new int[size];
-      Random ran = new Random();
+      int size = 20;
+      int randomBound = 100;
 
-      for (int i = 0; i < nums.length; i++)
-      {
-         nums[i] = ran.nextInt(1000);
-      }
-      for (int i : nums)
-      {
-         System.out.print(i + " ");
-      }
-      sort(nums);
-      System.out.println();
-      System.out.println();
-      for (int i : nums)
-      {
-         System.out.print(i + " ");
-      }
+      BubbleSort sort = new BubbleSort();
+      //ASCENDING SORT
+      int[] nums = getRandomArray(size, randomBound);
+
+      System.out.println("UNSORTED ARRAY");
+      printArray(nums);
+
+      sort.sortAscending(nums);
+
+      System.out.println("SORTED ARRAY -- Ascending");
+      printArray(nums);
+
+      //DESCENDING SORT
+      nums = getRandomArray(size, randomBound);
+
+      System.out.println("UNSORTED ARRAY");
+      printArray(nums);
+
+      sort.sortDescending(nums);
+
+      System.out.println("SORTED ARRAY - Descending");
+      printArray(nums);
    }
 }
